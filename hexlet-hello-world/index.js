2212a3bc2162f0e4a89530b34c8ee03347d671d8
Егор Бажайкин <egor50311@gmail.com git config --global user.name zitraxbladegit commit -m 'hexlet'git branch -M maingit remote add origin git@github.com:zitraxblade/hexlet-js.gitgit push -u origin maingit remote add origin git@github.com:zitraxblade/hexlet-js.gitgit branch -M maingit push -u origin maingit statusE1sqirexxtentionmamaruss.50311LSPWD$ git config --global user.email egor50311@gmail.com>
2024-07-13 20:18:54 +0300

    prettie

--- hexlet-hello-world/index.js
+++ hexlet-hello-world/index.js
@@ -1,4 +1,3 @@
-
-console.log('Hello, Hexlet');
-import _ from "lodash"
-console.log(_.last(['one', 'two']));
+console.log("Hello, Hexlet");
+import _ from "lodash";
+console.log(_.last(["one", "two"]));
